75b7850c331150195163f87deaf982b2c3f0e8ab
David González Zornoza <dgzornoza@dgzornoza.com>
2023-05-09 20:09:40 +0200

    correccion webpack

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,14 +1,38 @@
+import { Command } from 'commander';
+import * as figlet from 'figlet';
 import inquirer from 'inquirer';
+var pjson = require('../package.json');
+
+// @ts-ignore
+import { default as standard } from 'figlet/importable-fonts/Standard';
+
+const devilmalUrl = 'devimalplanet.com';
+
 
 class Main {
+
+    private program;
+
     constructor() {
+
+        figlet.parseFont("Standard", standard);
+        console.log(figlet.textSync("Dir Manager"));
+        this.program = this.setupCommander();
+
         this.init();
     }
 
-    private async init() {
 
+    async init() {
+        var options = this.program.opts();
+        const ranWithArgs = options.skipPrompts || options.url;
+        if (!ranWithArgs) { return this.interactiveRun(); }
+
+        const url = typeof ranWithArgs === 'string' ? ranWithArgs : devilmalUrl;
+    }
+
+    async interactiveRun() {
         console.log('Hi! 👋  Welcome devimal-cli!');
-        let urlToVisit = 'devimalplanet.com';
 
         const { openDevimal } = await inquirer.prompt({
             type: 'confirm',
@@ -16,25 +40,39 @@ class Main {
             message: 'Would you like to visit devimalplanet.com?',
             default: true
         });
-        console.log(openDevimal);
 
-        if (!openDevimal) {
-            // not opening devimalplanet.com
-            const { someFunUrl } = await inquirer.prompt({
-                type: 'input',
-                name: 'someFunUrl',
-                message: '😢  No? Which URL would you like to visit?',
-                validate: function (input) {
-                    return (
-                        /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/.test(
-                            input
-                        ) || 'Please enter a valid URL.'
-                    );
-                }
-            });
-            urlToVisit = someFunUrl;
-        }
-        console.log(urlToVisit);
+        const urlToVisit = openDevimal
+            ? devilmalUrl
+            : (
+                await inquirer.prompt({
+                    type: 'input',
+                    name: 'someFunUrl',
+                    message: '😢  No? Which URL would you like to visit?',
+                    validate: function (input) {
+                        return (
+                            /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/.test(
+                                input
+                            ) || 'Please enter a valid URL.'
+                        );
+                    }
+                })
+            ).someFunUrl;
+    }
+
+    setupCommander() {
+
+        const program = new Command();
+
+        program
+            .version(pjson.version)
+            .option(
+                '-y, --skip-prompts',
+                'skips questions, directly opens devimalplanet.com'
+            )
+            .option('-u, --url <URL>', 'skips questions, directly opens provided URL')
+            .parse(process.argv);
+
+        return program;
     }
 }
 
--- cli/webpack.config.js
+++ cli/webpack.config.js
@@ -1,38 +1,24 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.ts",
-  devtool: "source-map",
-  stats: "errors-warnings",
-  target: "node",
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  stats: 'errors-warnings',
+  target: 'node',
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "zx-cli.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-      }),
-    ],
+    filename: 'zx-ide-cli.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
